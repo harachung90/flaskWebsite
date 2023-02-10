960687221d6dc7cf06393b7fc6563d9df32a409c
Hara <harachung90@gmail.com>
2023-02-10 19:09:47 +1100

    Initial commit

---
+++ views.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template, request, jsonify
+
+views = Blueprint(__name__, "views")
+
+
+@views.route("/")
+def home():
+    return render_template("index.html", name="Hara")
+
+@views.route("/profile")
+def profile():
+    args = request.args
+    name = args.get('name')
+    return render_template("index.html", name=name)
+
+@views.route("/json")
+def get_json():
+    return jsonify({'name': 'Hara', 'coolness': 10})
